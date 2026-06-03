2f23e7cbec5397ca74f7f5f133eb07d99a8d6447
Mayur Bohra <mayurbohra971@gmail.com>
2026-06-03 23:14:57 +0530

    second commit2

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -6,7 +6,6 @@ import { Link, Container, Typography, Divider, Stack, Button } from '@mui/materi
 // hooks
 import useResponsive from '../hooks/useResponsive';
 // components
-import { ReactComponent as MatchKaro } from '../components/logo/logo.png';
 import Iconify from '../components/iconify';
 // sections
 import { LoginForm } from '../sections/auth/login';
